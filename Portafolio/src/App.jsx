897f9ec3c1e73f1eaa0b5135ebe95f4a611aaf10
xavierjosefs <xavierfernandezsantana.com>
2025-11-16 00:30:01 -0400

    Fix: about work

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from './components/organism/NavBar';
 import Hero from './components/organism/Hero';
 import Slogan from './components/organism/Slogan';
 import AboutMe from './components/organism/AboutMe';
+import Areas from './components/molecules/Areas';
 
 export default function App() {
   return (
--- Portafolio/src/components/molecules/Areas.jsx
+++ Portafolio/src/components/molecules/Areas.jsx
@@ -1,7 +1,23 @@
+import AreaCard from "../atoms/AreaCard"
+import diseno from "../../assets/images/diseno.png"
+import dev from "../../assets/images/dev.png"
+import maintence from "../../assets/images/maintence.png"
+
+
 export default function Areas() {
   return (
-    <div>
-      
+    <div className="pt-10 md:pt-16">
+      <div className="flex flex-col justify-center items-center lg:flex-row lg:gap-32">
+        <AreaCard title="DESIGN" img={diseno} text="I design clean, modern and user-focused interfaces that balance aesthetics and functionality."/>
+        <AreaCard title="DEVELOPMENT" img={dev} text="I build fast, scalable applications using Node.js and Express, focused on clean architecture and reliable API integrations."/>
+      </div>
+
+      <div className="flex justify-center md:pt-10">
+        <AreaCard title="MAINTENANCE" img={maintence} text="I maintain applications with updates, security patches, and performance improvements so your systems stay stable and reliable over time."/>
+      </div>
     </div>
+    
+    
+      
   )
 }
